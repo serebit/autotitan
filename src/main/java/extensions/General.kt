cf784fbb15365bd97de0bbefbaab7868166e9489
Campbell Jones <serebitmail@gmail.com>
2017-04-04 12:01:46 -0400

    Working on adding commands to General extension

--- src/main/java/extensions/General.kt
+++ src/main/java/extensions/General.kt
@@ -1,12 +1,19 @@
 package extensions
 
 import annotation.Command
+import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.entities.Message
+import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 
 class General {
   @Command()
   fun test(evt: MessageReceivedEvent) {
     evt.channel.sendMessage("Hello World!").queue()
+  }
+  
+  @Command()
+  fun memberInfo(evt: MessageReceivedEvent, member: Member) {
+    val embedBuilder = EmbedBuilder()
   }
 }
