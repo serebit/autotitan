e95590c01e7cc189d607858af0f473deb7b54ccb
Campbell Jones <serebitmail@gmail.com>
2017-04-04 16:30:47 +0000

    Fleshing out memberInfo command

--- src/main/java/extensions/General.kt
+++ src/main/java/extensions/General.kt
@@ -2,7 +2,9 @@ package extensions
 
 import annotation.Command
 import net.dv8tion.jda.core.EmbedBuilder
+import net.dv8tion.jda.core.entities.Member
 import net.dv8tion.jda.core.entities.Message
+import net.dv8tion.jda.core.entities.MessageEmbed
 import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 
@@ -14,6 +16,12 @@ class General {
   
   @Command()
   fun memberInfo(evt: MessageReceivedEvent, member: Member) {
+    val user = member.user
     val embedBuilder = EmbedBuilder()
+    var title = "User info for " + user.name + user.discriminator
+    if (member.nickname != null) title += " (also known as " + member.nickname + ")"
+    embedBuilder.setTitle(title, null)
+    embedBuilder.color = member.color
+    embedBuilder.thumbnail = user.effectiveAvatarUrl
   }
 }
