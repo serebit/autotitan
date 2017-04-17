298fbfc901629c996fc8ef627f1bcbc3576f833e
Campbell Jones <serebitmail@gmail.com>
2017-04-17 14:22:22 -0400

    Added bot and user permission fields to Command and Listener annotations.

--- src/main/java/com/serebit/autotitan/annotations/CommandFunction.kt
+++ src/main/java/com/serebit/autotitan/annotations/CommandFunction.kt
@@ -1,9 +1,13 @@
 package com.serebit.autotitan.annotations
 
+import net.dv8tion.jda.core.Permission
+
 @Target(AnnotationTarget.FUNCTION)
 annotation class CommandFunction(
     val name: String = "",
     val description: String = "",
     val delimitFinalParameter: Boolean = true,
-    val serverOnly: Boolean = false
+    val serverOnly: Boolean = false,
+    val botPermissions: Array<Permission> = arrayOf(),
+    val userPermissions: Array<Permission> = arrayOf()
 )
--- src/main/java/com/serebit/autotitan/annotations/ListenerFunction.kt
+++ src/main/java/com/serebit/autotitan/annotations/ListenerFunction.kt
@@ -1,8 +1,11 @@
 package com.serebit.autotitan.annotations
 
+import net.dv8tion.jda.core.Permission
+
 @Target(AnnotationTarget.FUNCTION)
 annotation class ListenerFunction(
     val name: String = "",
     val description: String = "",
-    val serverOnly: Boolean = false
+    val serverOnly: Boolean = false,
+    val botPermissions: Array<Permission> = arrayOf()
 )
