e2332af02b14ccdd7458fa4aeaa3929d2c0a411f
Campbell Jones <serebitmail@gmail.com>
2017-05-16 13:47:50 -0400

    Deleted useless data class in audio package.

--- src/main/java/com/serebit/autotitan/extensions/audio/Data.kt
+++
@@ -1,6 +0,0 @@
-package com.serebit.autotitan.extensions.audio
-
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack
-
-data class Data(val player: AudioPlayer, var playingTrack: AudioTrack?)
