4021b8660c62b8fb8b04aba216e7b708a0a87283
Campbell Jones <serebitmail@gmail.com>
2017-07-19 18:10:04 -0400

    Added Twitter4J to dependencies.

---
+++ src/main/java/com/serebit/autotitan/extensions/extra/Twitter.kt
@@ -0,0 +1,10 @@
+package com.serebit.autotitan.extensions.extra
+
+import com.serebit.autotitan.api.annotations.CommandFunction
+import com.serebit.autotitan.api.annotations.ExtensionClass
+
+@ExtensionClass
+class Twitter {
+    @CommandFunction
+    fun
+}
