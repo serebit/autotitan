213d239ba528458e80c6c389048aaf8bccb1cefc
Campbell Jones <serebitmail@gmail.com>
2017-07-22 01:55:45 -0400

    Added Twitter extension.

--- src/main/java/com/serebit/autotitan/config/Configuration.kt
+++ src/main/java/com/serebit/autotitan/config/Configuration.kt
@@ -6,52 +6,78 @@ import java.io.File
 import java.util.*
 
 class Configuration {
-  private val file: File
-  var token: String
-    internal set
-  var prefix: String = "!"
-    set(value) {
-      field = when {
-        value.isEmpty() -> "!"
-        value.length in 1..3 -> value
-        else -> value.substring(0..3)
-      }
-      serialize()
+    private val file: File
+    var token: String
+        internal set
+    var prefix: String = "!"
+        set(value) {
+            field = when {
+                value.isEmpty() -> "!"
+                value.length in 1..3 -> value
+                else -> value.substring(0..3)
+            }
+        }
+    val blackList: MutableSet<User>
+    val oAuthConsumerKey: String
+    val oAuthConsumerSecret: String
+    val oAuthAccessToken: String
+    val oAuthAccessTokenSecret: String
+
+    init {
+        val parentFolder = File(this::class.java.protectionDomain.codeSource.location.toURI())
+                .parentFile
+        file = File("$parentFolder/data/config.json")
+        if (file.exists()) {
+            val configData = Gson().fromJson(file.readText(), ConfigurationData::class.java)
+            token = configData.token
+            prefix = configData.prefix
+            blackList = configData.blackList
+            oAuthConsumerKey = configData.oAuthConsumerKey
+            oAuthConsumerSecret = configData.oAuthConsumerSecret
+            oAuthAccessToken = configData.oAuthAccessToken
+            oAuthAccessTokenSecret = configData.oAuthAccessTokenSecret
+        } else {
+            token = prompt("Enter new token:")
+            prefix = prompt("Enter new command prefix:")
+            blackList = mutableSetOf()
+            oAuthConsumerKey = prompt("Enter Twitter OAuth Consumer Key:")
+            oAuthConsumerSecret = prompt("Enter Twitter OAuth Consumer Secret:")
+            oAuthAccessToken = prompt("Enter Twitter OAuth Access Token:")
+            oAuthAccessTokenSecret = prompt("Enter Twitter OAuth Access Token Secret:")
+            serialize()
+        }
     }
-  var blackList: MutableSet<User> = mutableSetOf()
-    private set
 
-  init {
-    val parentFolder = File(this::class.java.protectionDomain.codeSource.location.toURI()).parentFile
-    file = File("$parentFolder/data/config.json")
-    if (file.exists()) {
-      val configData = Gson().fromJson(file.readText(), ConfigurationData::class.java)
-      token = configData.token
-      prefix = configData.prefix
-      blackList = configData.blackList
-    } else {
-      token = prompt("Enter new token:")
-      prefix = prompt("Enter new command prefix:")
-      serialize()
+    internal fun serialize() {
+        file.parentFile.mkdirs()
+        file.writeText(Gson().toJson(
+                ConfigurationData(
+                        token,
+                        prefix,
+                        blackList,
+                        oAuthConsumerKey,
+                        oAuthConsumerSecret,
+                        oAuthAccessToken,
+                        oAuthAccessTokenSecret
+                )
+        ))
     }
-  }
 
-  internal fun serialize() {
-    file.parentFile.mkdirs()
-    file.writeText(Gson().toJson(ConfigurationData(token, prefix, blackList)))
-  }
+    private fun prompt(prompt: String): String {
+        print("$prompt\n> ")
+        val input = Scanner(System.`in`).nextLine().trim()
+        return if (input.contains("\n") || input.contains(" ")) {
+            prompt(prompt)
+        } else input
+    }
 
-  private fun prompt(prompt: String): String {
-    print("$prompt\n> ")
-    val input = Scanner(System.`in`).nextLine().trim()
-    return if (input.contains("\n") || input.contains(" ")) {
-      prompt(prompt)
-    } else input
-  }
-
-  private data class ConfigurationData(
-      val token: String,
-      val prefix: String,
-      val blackList: MutableSet<User>
-  )
+    private data class ConfigurationData(
+            val token: String,
+            val prefix: String,
+            val blackList: MutableSet<User>,
+            val oAuthConsumerKey: String,
+            val oAuthConsumerSecret: String,
+            val oAuthAccessToken: String,
+            val oAuthAccessTokenSecret: String
+    )
 }
---
+++ src/main/java/com/serebit/autotitan/extensions/extra/TwitterExtension.kt
@@ -0,0 +1,85 @@
+package com.serebit.autotitan.extensions.extra
+
+import com.serebit.autotitan.api.Locale
+import com.serebit.autotitan.api.annotations.CommandFunction
+import com.serebit.autotitan.api.annotations.ExtensionClass
+import com.serebit.autotitan.config
+import net.dv8tion.jda.core.EmbedBuilder
+import net.dv8tion.jda.core.entities.TextChannel
+import net.dv8tion.jda.core.entities.User
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import twitter4j.Twitter
+import twitter4j.TwitterFactory
+import twitter4j.conf.ConfigurationBuilder
+import java.util.*
+import kotlin.concurrent.timer
+
+
+@ExtensionClass
+class TwitterExtension {
+    private val twitter: Twitter
+    private val scheduler: Timer
+    private val tweetQueue = mutableListOf<Tweet>()
+
+    init {
+        val cb = ConfigurationBuilder()
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(config.oAuthConsumerKey)
+                .setOAuthConsumerSecret(config.oAuthConsumerSecret)
+                .setOAuthAccessToken(config.oAuthAccessToken)
+                .setOAuthAccessTokenSecret(config.oAuthAccessTokenSecret)
+        twitter = TwitterFactory(cb.build()).instance
+        scheduler = timer(daemon = true, period = 60000, initialDelay = 60000) {
+            if (tweetQueue.isNotEmpty()) {
+                val tweet = tweetQueue.first()
+                tweetQueue.removeAt(0)
+                val status = twitter.updateStatus(tweet.message)
+                val url = "https://twitter.com/${status.user.screenName}/status/${status.id}"
+                val builder = EmbedBuilder().apply {
+                    setAuthor(
+                            "Tweet by ${tweet.user.name}#${tweet.user.discriminator}",
+                            url,
+                            tweet.user.effectiveAvatarUrl
+                    )
+                    setDescription(tweet.message)
+                }
+                tweet.channel.sendMessage(builder.build()).queue()
+            }
+        }
+    }
+
+    @CommandFunction(locale = Locale.GUILD, delimitFinalParameter = false)
+    fun tweet(evt: MessageReceivedEvent, message: String) {
+        if (message.length <= 140) {
+            val tweet = Tweet(evt.textChannel, evt.author, message)
+            tweetQueue.add(tweet)
+            evt.channel.sendMessage(
+                    """${evt.author.asMention}, your tweet has been queued."""
+            ).queue()
+        } else {
+            evt.channel.sendMessage(
+                    """${evt.author.asMention}, your tweet is ${message.length - 140} characters
+                    too long!"""
+            ).queue()
+        }
+    }
+
+    @CommandFunction(locale = Locale.GUILD)
+    fun tweetQueue(evt: MessageReceivedEvent) {
+        val builder = EmbedBuilder().apply {
+            setTitle("Tweet Queue")
+            var description = ""
+            tweetQueue.forEach {
+                description += "${it.user.asMention}: \"${it.message}\"\n"
+            }
+            setDescription(description)
+        }
+        evt.channel.sendMessage(builder.build()).queue()
+    }
+
+    private data class Tweet(
+            val channel: TextChannel,
+            val user: User,
+            val message: String
+    )
+}
