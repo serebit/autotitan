202a6e2d7fd8e241933eb813d66448b70f5fb981
Campbell Jones <serebitmail@gmail.com>
2017-08-24 12:16:37 -0400

    Added data manager class.

---
+++ src/main/java/com/serebit/autotitan/data/DataManager.kt
@@ -0,0 +1,17 @@
+package com.serebit.autotitan.data
+
+import com.google.gson.GsonBuilder
+import java.io.File
+
+private val parentFolder = File(DataManager::class.java.protectionDomain.codeSource.location.toURI()).parentFile
+private val serializer = GsonBuilder().apply {
+    serializeNulls()
+}.create()
+
+class DataManager(type: Class<*>) {
+    private val dataFolder = File("$parentFolder/${type.name.replace(".", "/")}/")
+
+    fun read(fileName: String, objType: Class<*>) = serializer.fromJson("$dataFolder/$fileName", objType)
+
+    fun write(fileName: String, obj: Any) = File("$dataFolder/$fileName").writeText(serializer.toJson(obj))
+}
