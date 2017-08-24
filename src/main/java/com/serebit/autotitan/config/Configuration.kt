05dc46ebc343996d789db2c0bcf8874476554dc2
Campbell Jones <serebitmail@gmail.com>
2017-08-24 13:22:08 -0400

    Changed blackList type to custom set.

--- src/main/java/com/serebit/autotitan/config/Configuration.kt
+++ src/main/java/com/serebit/autotitan/config/Configuration.kt
@@ -1,6 +1,7 @@
 package com.serebit.autotitan.config
 
 import com.google.gson.Gson
+import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.entities.User
 import java.io.File
 import java.util.*
@@ -8,7 +9,7 @@ import java.util.*
 private data class ConfigurationData(
         val token: String,
         val prefix: String,
-        val blackList: MutableSet<User>
+        val blackList: BlackList
 )
 
 object Configuration {
@@ -18,7 +19,7 @@ object Configuration {
         internal set
     var prefix: String
         internal set
-    val blackList: MutableSet<User>
+    val blackList: BlackList
 
     init {
         if (file.exists()) {
@@ -29,7 +30,7 @@ object Configuration {
         } else {
             token = prompt("Enter new token:")
             prefix = prompt("Enter new command prefix:")
-            blackList = mutableSetOf()
+            blackList = BlackList()
             serialize()
         }
     }
@@ -44,5 +45,41 @@ object Configuration {
         return if (input.isBlank()) {
             prompt(text)
         } else input
+    }
+}
+
+class BlackList internal constructor() {
+    private val set = mutableSetOf<String>()
+
+    val size get() = set.size
+
+    fun contains(element: User) = element.id in set
+
+    fun containsAll(elements: Collection<User>) = set.containsAll(elements.map(User::getId))
+
+    fun isEmpty() = set.isEmpty()
+
+    fun add(element: User) = set.add(element.id)
+
+    fun addAll(elements: Collection<User>) = set.addAll(elements.map(User::getId))
+
+    fun clear() = set.clear()
+
+    fun iterator(jda: JDA) = Iterator(jda)
+
+    fun remove(element: User) = set.remove(element.id)
+
+    fun removeAll(elements: Collection<User>) = set.removeAll(elements.map(User::getId))
+
+    fun retainAll(elements: Collection<User>) = set.retainAll(elements.map(User::getId))
+
+    inner class Iterator(private val jda: JDA) : MutableIterator<User> {
+        private val iterator = set.iterator()
+
+        override fun hasNext() = iterator.hasNext()
+
+        override fun next(): User = jda.getUserById(iterator.next())
+
+        override fun remove() = iterator.remove()
     }
 }
