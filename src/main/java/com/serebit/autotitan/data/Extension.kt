2b72f9660735e86ca7f05936f65cfce99b8ea936
Campbell Jones <serebitmail@gmail.com>
2017-10-20 17:32:18 -0400

    Fixed extension loading

--- src/main/java/com/serebit/autotitan/data/Extension.kt
+++ src/main/java/com/serebit/autotitan/data/Extension.kt
@@ -2,25 +2,21 @@ package com.serebit.autotitan.data
 
 import com.google.common.reflect.ClassPath
 
-val extensions by lazy {
-    ClassPath
-            .from(Thread.currentThread().contextClassLoader)
-            .getTopLevelClassesRecursive("com.serebit.autotitan.extensions")
-            .mapNotNull { generateExtension(it.load()) }
-}
-val commands by lazy {
-    extensions.map { instance ->
-        instance::class.java.methods.mapNotNull { Command.generate(instance, it) }
-    }.flatten().toSet()
-}
-val listeners by lazy {
-    extensions.map { instance ->
-        instance::class.java.methods.mapNotNull { Listener.generate(instance, it) }
-    }.flatten().toSet()
-}
+val extensions = ClassPath
+        .from(Thread.currentThread().contextClassLoader)
+        .getTopLevelClassesRecursive("com.serebit.autotitan.modules")
+        .mapNotNull { generateExtension(it.load()) }
+
+val commands = extensions.map { instance ->
+    instance::class.java.methods.mapNotNull { Command.generate(instance, it) }
+}.flatten().toSet()
+
+val listeners = extensions.map { instance ->
+    instance::class.java.methods.mapNotNull { Listener.generate(instance, it) }
+}.flatten().toSet()
 
 private fun <T> generateExtension(extension: Class<T>): T? {
     if (extension.constructors.none { it.parameterCount == 0 }) return null
     if (extension.methods.none { Command.isValid(it) || Listener.isValid(it) }) return null
-    return extension.newInstance()
+    return extension.getConstructor().newInstance()
 }
