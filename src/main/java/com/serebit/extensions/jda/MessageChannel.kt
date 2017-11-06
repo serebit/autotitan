c8c8859017617a58d3d70b3c7647a775fd5ef353
Campbell Jones <serebitmail@gmail.com>
2017-11-05 19:18:25 -0500

    Added MessageChannel extension function

---
+++ src/main/java/com/serebit/extensions/jda/MessageChannel.kt
@@ -0,0 +1,10 @@
+package com.serebit.extensions.jda
+
+import net.dv8tion.jda.core.EmbedBuilder
+import net.dv8tion.jda.core.entities.Message
+import net.dv8tion.jda.core.entities.MessageChannel
+import net.dv8tion.jda.core.requests.RestAction
+
+fun MessageChannel.sendEmbed(init: EmbedBuilder.() -> Unit): RestAction<Message> {
+    return sendMessage(EmbedBuilder().apply(init).build())
+}
