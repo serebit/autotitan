4b011ba220dc0f8d49f32f407303bbc598c7d071
Campbell Jones <serebitmail@gmail.com>
2017-11-05 19:20:42 -0500

    Deleted old Jda extension functions file

--- src/main/java/com/serebit/autotitan/extensions/Jda.kt
+++
@@ -1,9 +0,0 @@
-package com.serebit.autotitan.extensions
-
-import net.dv8tion.jda.core.AccountType
-import net.dv8tion.jda.core.JDA
-import net.dv8tion.jda.core.JDABuilder
-
-fun jda(accountType: AccountType, init: JDABuilder.() -> Unit): JDA {
-    return JDABuilder(accountType).apply(init).buildBlocking()
-}
