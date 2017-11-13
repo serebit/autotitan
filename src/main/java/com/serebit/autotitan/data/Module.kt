b5be55a36183a94060ca327744e0a09fc0cf2b79
Campbell Jones <serebitmail@gmail.com>
2017-11-12 19:09:09 -0500

    Renamed Extension.kt to Module.kt

--- src/main/java/com/serebit/autotitan/data/Module.kt
+++ src/main/java/com/serebit/autotitan/data/Module.kt
@@ -2,20 +2,20 @@ package com.serebit.autotitan.data
 
 import com.google.common.reflect.ClassPath
 
-val extensions = ClassPath
+val modules = ClassPath
         .from(Thread.currentThread().contextClassLoader)
         .getTopLevelClassesRecursive("com.serebit.autotitan.modules")
-        .mapNotNull { generateExtension(it.load()) }
+        .mapNotNull { generateModule(it.load()) }
 
-val commands = extensions.map { instance ->
+val commands = modules.map { instance ->
     instance::class.java.methods.mapNotNull { Command.generate(instance, it) }
 }.flatten().toSet()
 
-val listeners = extensions.map { instance ->
+val listeners = modules.map { instance ->
     instance::class.java.methods.mapNotNull { Listener.generate(instance, it) }
 }.flatten().toSet()
 
-private fun <T> generateExtension(extension: Class<T>): T? {
+private fun <T> generateModule(extension: Class<T>): T? {
     if (extension.constructors.none { it.parameterCount == 0 }) return null
     if (extension.methods.none { Command.isValid(it) || Listener.isValid(it) }) return null
     return extension.getConstructor().newInstance()
