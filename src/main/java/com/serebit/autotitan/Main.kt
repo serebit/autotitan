cdf568a5c90cc057d885e0e45a6cf91af3ec4d8f
Campbell Jones <serebitmail@gmail.com>
2017-11-14 17:50:44 -0500

    Refactored main class. commands and listeners properties are now fieldless global properties

--- src/main/java/com/serebit/autotitan/Main.kt
+++ src/main/java/com/serebit/autotitan/Main.kt
@@ -7,13 +7,28 @@ import com.serebit.autotitan.api.generateModule
 import com.serebit.autotitan.listeners.EventListener
 import com.serebit.extensions.jda.jda
 import net.dv8tion.jda.core.AccountType
-import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.core.events.Event
 
 const val NAME = "AutoTitan"
 const val VERSION = "0.3.2"
+private val modules = ClassPath
+        .from(Thread.currentThread().contextClassLoader)
+        .getTopLevelClassesRecursive("com.serebit.autotitan.modules")
+        .mapNotNull { generateModule(it.load()) }
+private val commands
+    get() = modules.map { instance ->
+        instance::class.java.methods.mapNotNull { method -> Command.generate(instance, method) }
+    }.flatten()
+private val listeners
+    get() = modules.map { instance ->
+        instance::class.java.methods.mapNotNull { method -> Listener.generate(instance, method) }
+    }.flatten()
 
 fun main(args: Array<String>) {
-    val jda = initJda()
+    val jda = jda(AccountType.BOT) {
+        setToken(config.token)
+        addEventListener(EventListener(commands, listeners))
+    }
 
     println()
     println("$NAME v$VERSION")
@@ -22,19 +37,9 @@ fun main(args: Array<String>) {
     println("Invite link: ${jda.asBot().getInviteUrl()}")
 }
 
-private fun initJda(): JDA {
-    val modules = ClassPath
-            .from(Thread.currentThread().contextClassLoader)
-            .getTopLevelClassesRecursive("com.serebit.autotitan.modules")
-            .mapNotNull { generateModule(it.load()) }
-    val commands = modules.map { instance ->
-        instance::class.java.methods.mapNotNull { method -> Command.generate(instance, method) }
-    }.flatten()
-    val listeners = modules.map { instance ->
-        instance::class.java.methods.mapNotNull { method -> Listener.generate(instance, method) }
-    }.flatten()
-    return jda(AccountType.BOT) {
-        setToken(config.token)
-        addEventListener(EventListener(commands, listeners))
+fun resetJda(evt: Event) {
+    evt.run {
+        jda.registeredListeners.forEach { jda.removeEventListener(it) }
+        jda.addEventListener(EventListener(commands, listeners))
     }
 }
