e32f95b4943f48ce949bd4e9f1f69d0306cd227e
Campbell Jones <serebitmail@gmail.com>
2017-11-14 17:10:07 -0500

    Added more precision to checking initial token and prefix input

--- src/main/java/com/serebit/autotitan/Configuration.kt
+++ src/main/java/com/serebit/autotitan/Configuration.kt
@@ -11,9 +11,9 @@ class Configuration private constructor(
         var prefix: String,
         val blackList: MutableSet<Long>
 ) {
-    internal fun serialize() = file.apply { createNewFile() }.writeText(Gson().toJson(this))
+    fun serialize() = file.apply { createNewFile() }.writeText(Gson().toJson(this))
 
-    internal companion object {
+    companion object {
         private val parentFolder = File(this::class.java.protectionDomain.codeSource.location.toURI()).parentFile
         val file = File("$parentFolder/.config")
 
@@ -21,17 +21,17 @@ class Configuration private constructor(
             Gson().fromJson(file.readText(), Configuration::class.java)
         } else {
             Configuration(
-                    token = prompt("Enter new token:"),
-                    prefix = prompt("Enter new command prefix:"),
+                    token = prompt("Enter new token:") { !it.contains("\\s".toRegex()) },
+                    prefix = prompt("Enter new command prefix:") { !it.contains("\\s".toRegex()) },
                     blackList = mutableSetOf()
             ).apply { serialize() }
         }
 
-        private fun prompt(text: String): String {
+        private fun prompt(text: String, condition: (String) -> Boolean): String {
             print("$text\n> ")
             val input = Scanner(System.`in`).nextLine().trim()
-            return if (input.isBlank()) {
-                prompt(text)
+            return if (input.isBlank() || !condition(input)) {
+                prompt(text, condition)
             } else input
         }
     }
