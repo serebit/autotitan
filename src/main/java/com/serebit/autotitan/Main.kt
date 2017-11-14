8b63fbe654d110536962cb8b99c53c3a46430cea
Campbell Jones <serebitmail@gmail.com>
2017-11-14 17:11:39 -0500

    Extracted initJda function from main function

--- src/main/java/com/serebit/autotitan/Main.kt
+++ src/main/java/com/serebit/autotitan/Main.kt
@@ -7,29 +7,34 @@ import com.serebit.autotitan.api.generateModule
 import com.serebit.autotitan.listeners.EventListener
 import com.serebit.extensions.jda.jda
 import net.dv8tion.jda.core.AccountType
+import net.dv8tion.jda.core.JDA
 
 const val NAME = "AutoTitan"
 const val VERSION = "0.3.2"
-val modules = ClassPath
-        .from(Thread.currentThread().contextClassLoader)
-        .getTopLevelClassesRecursive("com.serebit.autotitan.modules")
-        .mapNotNull { generateModule(it.load()) }
-val commands = modules.map { instance ->
-    instance::class.java.methods.mapNotNull { Command.generate(instance, it) }
-}.flatten()
-val listeners = modules.map { instance ->
-    instance::class.java.methods.mapNotNull { Listener.generate(instance, it) }
-}.flatten()
 
 fun main(args: Array<String>) {
-    val jda = jda(AccountType.BOT) {
-        setToken(config.token)
-        addEventListener(EventListener(commands, listeners))
-    }
+    val jda = initJda()
 
     println()
     println("$NAME v$VERSION")
     println("Username:    ${jda.selfUser.name}")
     println("Ping:        ${jda.ping}ms")
     println("Invite link: ${jda.asBot().getInviteUrl()}")
+}
+
+private fun initJda(): JDA {
+    val modules = ClassPath
+            .from(Thread.currentThread().contextClassLoader)
+            .getTopLevelClassesRecursive("com.serebit.autotitan.modules")
+            .mapNotNull { generateModule(it.load()) }
+    val commands = modules.map { instance ->
+        instance::class.java.methods.mapNotNull { method -> Command.generate(instance, method) }
+    }.flatten()
+    val listeners = modules.map { instance ->
+        instance::class.java.methods.mapNotNull { method -> Listener.generate(instance, method) }
+    }.flatten()
+    return jda(AccountType.BOT) {
+        setToken(config.token)
+        addEventListener(EventListener(commands, listeners))
+    }
 }
