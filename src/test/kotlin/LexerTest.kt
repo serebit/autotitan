27e63806a01eb48a79aa6ddaafa0e4322c2fdcc9
Campbell Jones <serebitmail@gmail.com>
2017-11-20 23:49:22 -0500

    Improved LexerTest with random strings, floats and ints

--- src/test/kotlin/LexerTest.kt
+++ src/test/kotlin/LexerTest.kt
@@ -1,17 +1,30 @@
 import com.serebit.autotitan.api.parser.Token
 import com.serebit.autotitan.api.parser.Tokens
 import com.serebit.autotitan.api.parser.tokenizeMessage
-import io.kotlintest.matchers.shouldBe
+import io.kotlintest.properties.Gen
+import io.kotlintest.properties.forAll
 import io.kotlintest.specs.StringSpec
+import java.util.*
 
 class LexerTest : StringSpec() {
     init {
-        "check token" {
-            tokenizeMessage("!run 2 \"test string\"") shouldBe listOf(
-                    Token(Tokens.INVOCATION, "!run"),
-                    Token(Tokens.INTEGER, "2"),
-                    Token(Tokens.STRING, "\"test string\"")
-            )
+        "tests lexer parsing" {
+            forAll(
+                    Gen.default(),
+                    Gen.default(),
+                    StringGenerator()
+            ) { f: Float, i: Int, s: String ->
+                tokenizeMessage("!run $f $i \"$s\"") == listOf(
+                        Token(Tokens.INVOCATION, "!run"),
+                        Token(Tokens.FLOAT, f.toString()),
+                        Token(Tokens.INTEGER, i.toString()),
+                        Token(Tokens.STRING, "\"$s\"")
+                )
+            }
         }
     }
+}
+
+private class StringGenerator : Gen<String> {
+    override fun generate(): String = nextPrintableString(Random().nextInt(99) + 1)
 }
