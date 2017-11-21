b93b71623e9f6031a2dbb1fe9384706e5d4ae5c2
Campbell Jones <serebitmail@gmail.com>
2017-11-20 23:43:49 -0500

    tokenizeMessage function now recognizes all whitespace characters

--- src/main/java/com/serebit/autotitan/api/parser/Lexer.kt
+++ src/main/java/com/serebit/autotitan/api/parser/Lexer.kt
@@ -3,9 +3,10 @@ package com.serebit.autotitan.api.parser
 fun tokenizeMessage(message: String): List<Token> {
     val tokens = mutableListOf<Token>()
     var position = 0
-    val numWhitespaces = message.count { it == ' ' }
+    val numWhitespaces = message.count { it.isWhitespace() }
     (0..numWhitespaces).forEach {
-        val whitespaceIndex = if (it == numWhitespaces) message.length else message.indexOf(' ', position)
+        val whitespaceIndex = if (it == numWhitespaces) message.length
+        else message.drop(position).indexOfFirst { it.isWhitespace() } + position
         val substring = message.substring(position until whitespaceIndex)
         val tokenType = Tokens.values().firstOrNull { substring.matches(it.regex) } ?: return@forEach
         tokens.add(Token(tokenType, substring))
