e82e2091312519074bd637cfa752b280734719f4
Campbell Jones <serebitmail@gmail.com>
2017-11-28 17:05:28 -0500

    Moved TrackScheduler class into GuildMusicManager

--- src/main/java/com/serebit/autotitan/modules/standard/audio/GuildMusicManager.kt
+++ src/main/java/com/serebit/autotitan/modules/standard/audio/GuildMusicManager.kt
@@ -2,18 +2,64 @@ package com.serebit.autotitan.modules.standard.audio
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason
 
 /**
  * Holder for both the player and a track scheduler for one guild.
  */
 class GuildMusicManager(manager: AudioPlayerManager) {
     val player: AudioPlayer = manager.createPlayer()
-    val scheduler = TrackScheduler(player)
+    val scheduler = TrackScheduler()
     val sendHandler by lazy {
         AudioPlayerSendHandler(player)
     }
 
     init {
         player.addListener(scheduler)
+    }
+
+    inner class TrackScheduler : AudioEventAdapter() {
+        val queue = mutableListOf<AudioTrack>()
+
+        fun addToQueue(track: AudioTrack) {
+            if (player.playingTrack == null) {
+                player.playTrack(track)
+            } else {
+                queue.add(track)
+            }
+        }
+
+        fun skipTrack(): Boolean {
+            return if (player.playingTrack != null) {
+                player.stopTrack()
+                if (queue.isNotEmpty()) player.playTrack(queue.removeAt(0))
+                true
+            } else {
+                false
+            }
+        }
+
+        fun pause() = if (!player.isPaused) {
+            player.isPaused = true
+            true
+        } else false
+
+        fun resume() = if (player.isPaused) {
+            player.isPaused = false
+            true
+        } else false
+
+        fun stop() {
+            player.stopTrack()
+            queue.clear()
+        }
+
+        override fun onTrackEnd(player: AudioPlayer, track: AudioTrack, endReason: AudioTrackEndReason) {
+            if (queue.size >= 1 && endReason == AudioTrackEndReason.FINISHED) {
+                if (queue.isNotEmpty()) player.playTrack(queue.removeAt(0))
+            }
+        }
     }
 }
