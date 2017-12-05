47dfc5c4707a5b9cdc34bf39b05ea662257a5f81
Campbell Jones <serebitmail@gmail.com>
2017-12-04 21:48:52 -0500

    Fixed lack of return statement in generateModule

--- src/main/java/com/serebit/autotitan/api/Module.kt
+++ src/main/java/com/serebit/autotitan/api/Module.kt
@@ -3,7 +3,7 @@ package com.serebit.autotitan.api
 import com.serebit.autotitan.api.meta.annotations.Module as ModuleAnnotation
 
 fun <T : Any> generateModule(moduleClass: Class<T>): T? {
-    if (moduleClass.isAnnotationPresent(ModuleAnnotation::class.java))
+    if (!moduleClass.isAnnotationPresent(ModuleAnnotation::class.java)) return null
     if (moduleClass.constructors.none { it.parameterCount == 0 }) return null
     if (moduleClass.methods.none { Command.isValid(it) || Listener.isValid(it) }) return null
     return moduleClass.getConstructor().newInstance()
