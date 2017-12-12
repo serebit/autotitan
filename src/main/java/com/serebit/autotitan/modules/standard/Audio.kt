81d5687f55617d8109e37b2588cb6c740e27eb5a
Campbell Jones <serebitmail@gmail.com>
2017-12-11 19:25:15 -0500

    Moved Audio.kt out of its folder

--- src/main/java/com/serebit/autotitan/modules/standard/Audio.kt
+++ src/main/java/com/serebit/autotitan/modules/standard/Audio.kt
@@ -1,4 +1,4 @@
-package com.serebit.autotitan.modules.standard.audio
+package com.serebit.autotitan.modules.standard
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
