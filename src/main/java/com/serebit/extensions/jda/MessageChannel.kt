2df88ba2413b8f6ee0de07bbe2610b8802e2bf3d
Campbell Jones <serebitmail@gmail.com>
2017-12-13 22:34:57 -0500

    Inlined extension functions

--- src/main/java/com/serebit/extensions/jda/MessageChannel.kt
+++ src/main/java/com/serebit/extensions/jda/MessageChannel.kt
@@ -5,4 +5,4 @@ import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.entities.MessageChannel
 import net.dv8tion.jda.core.requests.RestAction
 
-fun MessageChannel.sendEmbed(init: EmbedBuilder.() -> Unit): RestAction<Message> = sendMessage(embed(init))
+inline fun MessageChannel.sendEmbed(init: EmbedBuilder.() -> Unit): RestAction<Message> = sendMessage(embed(init))
