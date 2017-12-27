7d4aa1fb56fe3c5d2a6caec1e7fb301138ab3f0e
Campbell Jones <serebitmail@gmail.com>
2017-12-26 23:54:23 -0500

    Added help command with commandName parameter

--- src/main/java/com/serebit/autotitan/listeners/EventListener.kt
+++ src/main/java/com/serebit/autotitan/listeners/EventListener.kt
@@ -2,19 +2,34 @@ package com.serebit.autotitan.listeners
 
 import com.serebit.autotitan.api.Command
 import com.serebit.autotitan.api.Listener
+import com.serebit.autotitan.api.meta.annotations.Module
 import com.serebit.autotitan.config
 import com.serebit.extensions.jda.sendEmbed
 import kotlinx.coroutines.experimental.launch
 import net.dv8tion.jda.core.events.Event
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
+import kotlin.reflect.full.declaredMemberFunctions
 import kotlin.reflect.full.instanceParameter
 import kotlin.reflect.jvm.jvmErasure
+import com.serebit.autotitan.api.meta.annotations.Command as CommandAnnotation
 
 class EventListener(
-        private val commands: Collection<Command>,
-        private val listeners: Collection<Listener>
+        commands: Collection<Command>,
+        listeners: Collection<Listener>
 ) : ListenerAdapter() {
+    private val commands = commands.toMutableList()
+    private val listeners = listeners.toMutableList()
+
+    init {
+        Help().let { helpModule ->
+            this.commands.addAll(
+                    helpModule::class.declaredMemberFunctions.mapNotNull { Command.generate(it, helpModule) }
+            )
+        }
+
+    }
+
     override fun onGenericEvent(evt: Event) {
         launch {
             listeners.filter { it.eventType == evt::class }.forEach {
@@ -32,10 +47,6 @@ class EventListener(
     }
 
     private fun runCommands(evt: MessageReceivedEvent) {
-        if (evt.message.rawContent == "${config.prefix}help") {
-            sendCommandList(evt)
-            return
-        }
         val (command, parameters) = commands.asSequence()
                 .filter { it.looselyMatches(evt.message.rawContent) }
                 .associate { it to it.parseTokensOrNull(evt) }.entries
@@ -43,17 +54,28 @@ class EventListener(
         command(evt, parameters!!)
     }
 
-    private fun sendCommandList(evt: MessageReceivedEvent) {
-        evt.run {
-            channel.sendEmbed {
-                setColor(guild?.selfMember?.color)
-                commands.sortedBy { it.name }
-                        .groupBy { it.function.instanceParameter?.type?.jvmErasure?.simpleName }.entries
-                        .sortedBy { it.key }
-                        .forEach { (extension, commands) ->
-                            addField(extension, commands.joinToString("\n") { it.helpMessage }, false)
-                        }
-            }.complete()
+    @Module
+    inner class Help {
+        @CommandAnnotation
+        fun help(evt: MessageReceivedEvent) {
+            evt.run {
+                channel.sendEmbed {
+                    setColor(guild?.selfMember?.color)
+                    commands.sortedBy { it.name }
+                            .groupBy { it.function.instanceParameter?.type?.jvmErasure?.simpleName }.entries
+                            .sortedBy { it.key }
+                            .forEach { (extension, commands) ->
+                                addField(extension, commands.joinToString("\n") { it.summary }, false)
+                            }
+                }.complete()
+            }
+        }
+
+        @CommandAnnotation
+        fun help(evt: MessageReceivedEvent, commandName: String) {
+            commands.firstOrNull { it.name == commandName }?.let { command ->
+                evt.channel.sendMessage(command.helpEmbed).complete()
+            }
         }
     }
 }
