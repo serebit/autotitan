2ccca348a4f993babbc3e50f3269007bd65085a7
Campbell Jones <serebitmail@gmail.com>
2018-04-23 13:33:11 -0400

    Lexer refactoring

--- src/main/kotlin/com/serebit/autotitan/api/parser/Lexer.kt
+++ src/main/kotlin/com/serebit/autotitan/api/parser/Lexer.kt
@@ -4,13 +4,14 @@ internal fun tokenizeMessage(message: String): List<Token> {
     val tokens = mutableListOf<Token>()
     var position = 0
     val numWhitespaces = message.count { it.isWhitespace() }
-    for (i in 0..numWhitespaces) {
+    (0..numWhitespaces).forEach { i ->
         val whitespaceIndex = if (i == numWhitespaces) message.length
         else message.drop(position).indexOfFirst { it.isWhitespace() } + position
         val substring = message.substring(position until whitespaceIndex)
-        val tokenType = TokenType.values().firstOrNull { substring.matches(it.regex) } ?: continue
-        tokens.add(Token(tokenType, substring))
-        position = whitespaceIndex + 1
+        TokenType.values().firstOrNull { substring.matches(it.regex) }?.let { tokenType ->
+            tokens.add(Token(tokenType, substring))
+            position = whitespaceIndex + 1
+        }
     }
     return tokens
 }
