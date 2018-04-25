fff7c015142d58f7a9c7efb9fedc38ded89171e4
Campbell Jones <serebitmail@gmail.com>
2018-04-25 12:18:51 -0400

    Optimized tokenizeMessage

--- src/main/kotlin/com/serebit/autotitan/api/parser/Lexer.kt
+++ src/main/kotlin/com/serebit/autotitan/api/parser/Lexer.kt
@@ -4,7 +4,7 @@ internal fun tokenizeMessage(message: String): List<Token> {
     val tokens = mutableListOf<Token>()
     var position = 0
     val numWhitespaces = message.count { it.isWhitespace() }
-    (0..numWhitespaces).forEach { i ->
+    for (i in 0..numWhitespaces) {
         val whitespaceIndex = if (i == numWhitespaces) message.length
         else message.drop(position).indexOfFirst { it.isWhitespace() } + position
         val substring = message.substring(position until whitespaceIndex)
