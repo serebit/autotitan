6cc021d526d9b54a9d9656fe1dfb461ebd91910a
Campbell Jones <me@serebit.com>
2018-05-09 08:38:42 -0400

    Modified some Number extensions to be more efficient

--- src/main/kotlin/com/serebit/extensions/Number.kt
+++ src/main/kotlin/com/serebit/extensions/Number.kt
@@ -11,33 +11,33 @@ private const val SECONDS_PER_MINUTE = 60
 private const val METRIC_BASE = 1000
 private const val PERCENT_FACTOR = 100
 
-fun Number.asMetricUnit(unit: String, base: Int = METRIC_BASE, decimalPoints: Int = 1): String = toDouble().let {
-    val baseAsDouble = base.toDouble()
-    val exponent = floor(log(it - 1, baseAsDouble)).roundToInt()
+fun Long.asMetricUnit(unit: String, base: Int = METRIC_BASE, decimalPoints: Int = 1): String {
+    val baseAsFloat = base.toFloat()
+    val exponent = floor(log(toFloat() - 1, baseAsFloat)).roundToInt()
     val unitPrefix = listOf("", "K", "M", "G", "T", "P", "E")[exponent]
-    "%.${decimalPoints}f $unitPrefix$unit".format(it / baseAsDouble.pow(exponent))
+    return "%.${decimalPoints}f $unitPrefix$unit".format(this / baseAsFloat.pow(exponent))
 }
 
-fun Number.toBasicTimestamp(): String = toInt().let {
-    val hours by lazy { it / SECONDS_PER_HOUR }
-    val minutes = it % SECONDS_PER_HOUR / SECONDS_PER_MINUTE
-    val seconds = it % SECONDS_PER_MINUTE
-    if (hours == 0) {
+fun Long.toBasicTimestamp(): String {
+    val hours by lazy { this / SECONDS_PER_HOUR }
+    val minutes = this % SECONDS_PER_HOUR / SECONDS_PER_MINUTE
+    val seconds = this % SECONDS_PER_MINUTE
+    return if (hours == 0L) {
         "%d:%02d".format(minutes, seconds)
     } else "%d:%02d:%02d".format(hours, minutes, seconds)
 }
 
-fun Number.toVerboseTimestamp(): String = toInt().let {
-    val days by lazy { "${it / SECONDS_PER_DAY}d" }
-    val hours by lazy { "${it % SECONDS_PER_DAY / SECONDS_PER_HOUR}h" }
-    val minutes by lazy { "${it % SECONDS_PER_HOUR / SECONDS_PER_MINUTE}m" }
-    val seconds = "${it % SECONDS_PER_MINUTE}s"
-    when {
-        it < SECONDS_PER_MINUTE -> seconds
-        it < SECONDS_PER_HOUR -> "$minutes $seconds"
-        it < SECONDS_PER_DAY -> "$hours $minutes $seconds"
+fun Long.toVerboseTimestamp(): String {
+    val days by lazy { "${this / SECONDS_PER_DAY}d" }
+    val hours by lazy { "${this % SECONDS_PER_DAY / SECONDS_PER_HOUR}h" }
+    val minutes by lazy { "${this % SECONDS_PER_HOUR / SECONDS_PER_MINUTE}m" }
+    val seconds = "${this % SECONDS_PER_MINUTE}s"
+    return when {
+        this < SECONDS_PER_MINUTE -> seconds
+        this < SECONDS_PER_HOUR -> "$minutes $seconds"
+        this < SECONDS_PER_DAY -> "$hours $minutes $seconds"
         else -> "$days $hours $minutes $seconds"
     }
 }
 
-fun Number.asPercentageOf(total: Number): Int = (toDouble() / total.toDouble() * PERCENT_FACTOR).roundToInt()
+fun Long.asPercentageOf(total: Long): Long = this / total * PERCENT_FACTOR
