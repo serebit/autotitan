0394d7e12a3c673cabb442d1f2b3cd90be1cdf29
Campbell Jones <me@serebit.com>
2019-01-15 16:08:56 -0500

    Fixed parsing of some arg types, and added new arg types

--- src/main/kotlin/api/parser/Parser.kt
+++ src/main/kotlin/api/parser/Parser.kt
@@ -6,6 +6,7 @@ import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.entities.IMentionable
 import net.dv8tion.jda.core.entities.Member
+import net.dv8tion.jda.core.entities.Role
 import net.dv8tion.jda.core.entities.TextChannel
 import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
@@ -19,14 +20,15 @@ internal object Parser {
                 is TokenType.OtherToken -> castOther(evt, type, token)
             }
         }
-        return if (null in castedTokens) emptyList() else castedTokens.requireNoNulls()
+        return if (null in castedTokens) null else castedTokens.requireNoNulls()
     }
 
     private fun castJdaMentionable(evt: MessageReceivedEvent, type: TokenType.JdaToken, token: String): IMentionable? =
         when (type) {
             TokenType.JdaToken.UserToken -> evt.jda.getUserByMention(token)
             TokenType.JdaToken.MemberToken -> evt.guild.getMemberByMention(token)
-            TokenType.JdaToken.ChannelToken -> evt.guild.getTextChannelByMention(token)
+            TokenType.JdaToken.TextChannelToken -> evt.guild.getTextChannelByMention(token)
+            TokenType.JdaToken.RoleToken -> evt.guild.getRoleByMention(token)
         }
 
     private fun castNumeral(type: TokenType.NumberToken, token: String): Number? = when (type) {
@@ -34,8 +36,10 @@ internal object Parser {
         TokenType.NumberToken.ShortToken -> token.toShortOrNull()
         TokenType.NumberToken.IntToken -> token.toIntOrNull()
         TokenType.NumberToken.LongToken -> token.toLongOrNull()
+        TokenType.NumberToken.BigIntToken -> token.toBigIntegerOrNull()
         TokenType.NumberToken.DoubleToken -> token.toDoubleOrNull()
         TokenType.NumberToken.FloatToken -> token.toFloatOrNull()
+        TokenType.NumberToken.BigDecimalToken -> token.toBigDecimalOrNull()
     }
 
     private fun castOther(evt: MessageReceivedEvent, type: TokenType.OtherToken, token: String): Any? = when (type) {
@@ -56,4 +60,7 @@ internal object Parser {
 
     private fun Guild.getTextChannelByMention(mention: String): TextChannel? =
         getTextChannelById(mention.removeSurrounding("<#", ">"))
+
+    private fun Guild.getRoleByMention(mention: String): Role? =
+        getRoleById(mention.removeSurrounding("<@&", ">"))
 }
--- src/main/kotlin/api/parser/TokenType.kt
+++ src/main/kotlin/api/parser/TokenType.kt
@@ -2,12 +2,14 @@ package com.serebit.autotitan.api.parser
 
 import com.serebit.autotitan.api.parameters.Emote
 import com.serebit.autotitan.api.parameters.LongString
-import net.dv8tion.jda.core.entities.Channel
-import net.dv8tion.jda.core.entities.IMentionable
 import net.dv8tion.jda.core.entities.Member
+import net.dv8tion.jda.core.entities.MessageChannel
+import net.dv8tion.jda.core.entities.Role
+import net.dv8tion.jda.core.entities.TextChannel
 import net.dv8tion.jda.core.entities.User
+import java.math.BigDecimal
+import java.math.BigInteger
 import kotlin.reflect.KClass
-import kotlin.reflect.full.isSubclassOf
 
 internal sealed class TokenType(val name: String, val signature: Regex) {
     sealed class NumberToken(name: String, signature: Regex) : TokenType(name, signature) {
@@ -15,34 +17,39 @@ internal sealed class TokenType(val name: String, val signature: Regex) {
         object ShortToken : NumberToken("Short", "\\d+?".toRegex())
         object IntToken : NumberToken("Int", "\\d+?".toRegex())
         object LongToken : NumberToken("Long", "\\d+?L?".toRegex())
+        object BigIntToken : NumberToken("BigInt", "\\d+?".toRegex())
         object FloatToken : NumberToken("Float", "\\d+?.\\d+?f?".toRegex())
         object DoubleToken : NumberToken("Double", "\\d+?.\\d+?".toRegex())
+        object BigDecimalToken : NumberToken("BigDecimal", "\\d+?.\\d+?".toRegex())
 
         companion object {
-            fun from(type: KClass<out Any>): NumberToken? = when (type) {
-                Byte::class -> ByteToken
-                Short::class -> ShortToken
-                Int::class -> IntToken
-                Long::class -> LongToken
-                Float::class -> FloatToken
-                Double::class -> DoubleToken
-                else -> null
-            }
+            val typeAssociations = mapOf(
+                Byte::class to ByteToken,
+                Short::class to ShortToken,
+                Int::class to IntToken,
+                Long::class to LongToken,
+                BigInteger::class to BigIntToken,
+                Float::class to FloatToken,
+                Double::class to DoubleToken,
+                BigDecimal::class to BigDecimalToken
+            )
         }
     }
 
     sealed class JdaToken(name: String, signature: Regex) : TokenType(name, signature) {
         object UserToken : JdaToken("User", "<?@?!?\\d+?>?".toRegex())
         object MemberToken : JdaToken("Member", "<?@?!?\\d+?>?".toRegex())
-        object ChannelToken : JdaToken("Channel", "<?#?!?\\d+?>?".toRegex())
+        object TextChannelToken : JdaToken("Channel", "<?#?!?\\d+?>?".toRegex())
+        object RoleToken : JdaToken("Role", "(?:<@&)?\\d+?>?".toRegex())
 
         companion object {
-            fun from(type: KClass<out Any>): JdaToken? = when {
-                type == User::class -> UserToken
-                type == Member::class -> MemberToken
-                type.isSubclassOf(Channel::class) -> ChannelToken
-                else -> null
-            }
+            val typeAssociations = mapOf(
+                User::class to UserToken,
+                Member::class to MemberToken,
+                TextChannel::class to TextChannelToken,
+                MessageChannel::class to TextChannelToken,
+                Role::class to RoleToken
+            )
         }
     }
 
@@ -54,22 +61,22 @@ internal sealed class TokenType(val name: String, val signature: Regex) {
         object CharToken : OtherToken("Char", "\\S".toRegex())
 
         companion object {
-            fun from(type: KClass<out Any>): OtherToken? = when (type) {
-                Emote::class -> EmoteToken
-                String::class -> StringToken
-                LongString::class -> LongStringToken
-                Boolean::class -> BooleanToken
-                Char::class -> CharToken
-                else -> null
-            }
+            val typeAssociations = mapOf(
+                Emote::class to EmoteToken,
+                String::class to StringToken,
+                LongString::class to LongStringToken,
+                Boolean::class to BooleanToken,
+                Char::class to CharToken
+            )
         }
     }
 
     companion object {
-        fun from(type: KClass<out Any>): TokenType? = when {
-            type.isSubclassOf(Number::class) -> NumberToken.from(type)
-            type.isSubclassOf(IMentionable::class) -> JdaToken.from(type)
-            else -> OtherToken.from(type)
+        fun from(type: KClass<out Any>): TokenType? = when (type) {
+            in NumberToken.typeAssociations -> NumberToken.typeAssociations[type]
+            in JdaToken.typeAssociations -> JdaToken.typeAssociations[type]
+            in OtherToken.typeAssociations -> OtherToken.typeAssociations[type]
+            else -> null
         }
     }
 }
