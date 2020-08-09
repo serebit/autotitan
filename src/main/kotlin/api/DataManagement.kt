89d5594c9586dd953ec86f53c3f647f79b020720
Campbell Jones <git@serebit.com>
2020-08-09 01:14:56 -0400

    Use Gson for data management again

--- src/main/kotlin/api/DataManagement.kt
+++ src/main/kotlin/api/DataManagement.kt
@@ -1,33 +1,36 @@
 package com.serebit.autotitan.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import com.serebit.autotitan.internal.classpathResource
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import java.lang.reflect.Type
 
 /*
  kotlinx.serialization needs to do a pass over classes marked with @Serializable during compile time, so because scripts
  don't have access to that, DataManager uses Gson instead.
  */
 class DataManager internal constructor(moduleName: String) {
-    val folder by lazy { classpathResource("data/$moduleName").also { it.mkdirs() } }
+    private val serializer: Gson = GsonBuilder().create()
+    private val folder by lazy { classpathResource("data/$moduleName").also { it.mkdirs() } }
 
     /*
      This function *needs* to be reified to work with Gson. Gson has issues with Kotlin generics, so if T is of a
      type that uses generics, and T isn't reified when passed to the TypeToken, Gson fails to cast the JSON to the
      input type and throws an exception. Keep it in.
      */
+    inline fun <reified T : Any> read(fileName: String): T? = read(fileName, object : TypeToken<T>() {}.type)
 
-    inline fun <reified T : Any> read(fileName: String): T? = folder.resolve(fileName).let { file ->
-        if (file.exists()) Json.decodeFromString(file.readText()) else null
+    fun <T : Any> read(fileName: String, type: Type): T? = folder.resolve(fileName).let { file ->
+        if (file.exists()) serializer.fromJson(file.readText(), type) else null
     }
 
     inline fun <reified T : Any> readOrDefault(fileName: String, defaultValue: () -> T) =
         read(fileName) ?: defaultValue()
 
-    inline fun <reified T : Any> write(fileName: String, obj: T) = folder.resolve(fileName)
+    fun write(fileName: String, obj: Any) = folder.resolve(fileName)
         .also { it.createNewFile() }
-        .writeText(Json.encodeToString(obj))
+        .writeText(serializer.toJson(obj))
 }
 
 class GuildResourceMap<K, V> : MutableMap<Long, MutableMap<K, V>> by mutableMapOf()
